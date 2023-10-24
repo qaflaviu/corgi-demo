7e4e74ca6264b6b1e70f4ec0f69c3a43fdd8ef5f
Flaviu Sabau <flaviu.sabau.qa@gmail.com>
2023-10-24 21:50:07 +0300

    first commit

---
+++ steps/world.ts
@@ -0,0 +1,25 @@
+import { After, Before, setDefaultTimeout } from "@cucumber/cucumber";
+import { Browser, chromium, Page } from "playwright";
+
+let page: Page;
+let browser: Browser;
+
+setDefaultTimeout(60000);
+
+Before(async () => {
+  try {
+    browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    page = await context.newPage();
+    await page.goto("https://www.demoblaze.com");
+  } catch (error) {
+    throw new Error(`failed navigation due to ${error}`);
+  }
+  return page;
+});
+
+After(async () => {
+  await browser.close();
+});
+
+export { page, browser };
